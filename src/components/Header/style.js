b886126082f7d2bd5a895320d1660622e4900007
Gustav Pursche <pursche@posteo.de>
2020-07-02 21:07:38 +0200

    Header: improve alignment

--- src/components/Header/style.js
+++ src/components/Header/style.js
@@ -3,6 +3,7 @@ import { css } from 'linaria';
 import { colors, fluid, fonts } from '../../style-tokens';
 
 export const header = css`
+  align-items: center;
   color: ${colors.white};
   display: flex;
   justify-content: space-between;
