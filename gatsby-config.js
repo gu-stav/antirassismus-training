2864b5f4f235d756c9dd5bd98a4d0bf5553371f8
Gustav Pursche <pursche@posteo.de>
2020-07-02 08:43:14 +0200

    Gatsby: add boilerplate

---
+++ gatsby-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  plugins: [
+    'gatsby-plugin-preact',
+    'gatsby-plugin-linaria',
+    'gatsby-plugin-netlify',
+    'gatsby-plugin-hooked-head',
+  ],
+};
---
+++ src/pages/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const HomePage = () => <p>Here it comes</p>;
+
+export default HomePage;
